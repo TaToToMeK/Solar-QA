27f26a24b0de73da7777103b3556c800437d6a8a
Tomek Tomaszewicz <ttgithub@tomaszewicz.eu>
2025-08-06 11:46:50 +0200

    zdefiniowanie funkcji main()

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from A1_extract.from_solarman import pull_all_solarman  # Import the function from solarmanpv module
+import logging
+logging.basicConfig(
+    level=logging.INFO,  # lub DEBUG, WARNING, ERROR, CRITICAL
+    format='%(asctime)s | %(levelname)s | %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+logger = logging.getLogger(__name__)
 
 
-if __name__ == "__main__":
+def main():
+    # Główna funkcja do pobierania raportów Solarman
     print("Pobieranie raportów Solarman dla wszystkich urządzeń w 2025 roku...")
     pull_all_solarman()
+
+if __name__ == "__main__":
+    main()
