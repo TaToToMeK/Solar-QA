db89b0285f239d978abef39c625f50f76b75c056
Tomek Tomaszewicz <ttgithub@tomaszewicz.eu>
2025-08-25 15:18:51 +0200

    ustawienie bieżącego katalogu dla wrapper_main -niezbędne dla crontab

--- wrapper_main.sh
+++ wrapper_main.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
+# to be used with cron job
+#00 */3 * * * /home/astek/PV-monitor/wrapper_main.sh >> /home/astek/PV-monitor/logs/cron.log 2>&1
 echo "----------------------------------------"
 date
-echo "Starting script..."
+echo "Starting main.py wrapper script..."
+DIR="$(cd "$(dirname "$0")" && pwd)"
+echo $DIR
+cd $DIR
 source .venv/bin/activate
 python main.py --log-level DEBUG
 date
-echo "Ending script..."
+echo "Closing main.py wrapper script..."
 echo "----------------------------------------"
